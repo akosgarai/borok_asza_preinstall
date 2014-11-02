594d20b35ab3fb3538845f81f776895dc1a462aa
Patrik <lpat01@gmail.com>
2014-11-02 12:56:30 +0100

    remove footer from admin pages

--- opencart_preinstall/admin/language/hungarian/common/footer.php
+++ opencart_preinstall/admin/language/hungarian/common/footer.php
@@ -1,4 +1,2 @@
 <?php
-// Text
-$_['text_footer'] = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' Minden jog fenntartva.<br />Version %s <br />Magyar fordítás a <a href="http://www.csillamvilag.com">CsillámVilág</a> által.';
 ?>
